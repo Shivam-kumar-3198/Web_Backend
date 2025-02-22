90d1018e80d38be4fcb0d75574063a531b6ae2c5
Shivam <thisisshivamkumar8840@gmail.com>
2025-02-22 10:09:42 +0530

    .

---
+++ Outsider/Lecture_05_express_js/script.js
@@ -0,0 +1,39 @@
+// Express-js Framework:
+// Introduction to Express.js.
+// Framework -> Konsi chig kaise karni hai resources provided(Express js)
+// express js ek npm package hai
+// framework
+// manages everything from receiving the request and giving the response
+
+//MERN STACK
+// Setting up a basic Express application.
+// Routing.
+// Middleware.
+// Request and response handling.
+// Error handling.
+/*
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
+*/
+const express = require("express");
+const app = express();
+
+// routes creates karna
+
+app.get("/", function (req, res) {
+  res.send("You are a champion and server starts here");
+});
+
+app.get("/profile", function (req, res) {
+  res.send("Champion coach is also working with nodemon server now ");
+});
+
+app.listen(3000);
+
+// nodemon help karta hai live reloading me
